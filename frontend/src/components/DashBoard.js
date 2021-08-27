4551696f7307db28d9884bba41d7a6eeef91eae3
TCG <ssibonokuhle@gmail.com>
2021-08-27 11:11:04 +0200

    Add navbar

---
+++ frontend/src/components/DashBoard.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { withRouter } from "react-router";
+import NavBar from "./NavBar";
+import { useSelector } from "react-redux";
+
+const DashBoard = () => {
+  const user = useSelector((state) => state.auth.user);
+
+  return (
+    <div className="dashboard">
+      <header>
+        <NavBar {...user} />
+      </header>
+      <h2>Kuhlr</h2>
+    </div>
+  );
+};
+
+export default DashBoard;
