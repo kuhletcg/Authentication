0cf0f7d634da5176842983200608f6d96a23ae1f
TCG <ssibonokuhle@gmail.com>
2021-09-10 10:50:12 +0200

    require email

---
+++ backend/model/user.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+    unique: true,
+    min: 6,
+    max: 255,
+  },
+  password: {
+    type: Number,
+    min: 6,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+let User = mongoose.model("User", userSchema);
+module.exports = User;
