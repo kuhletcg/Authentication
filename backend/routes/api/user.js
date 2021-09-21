1adac0241e78c332e452dbf41323f739b39c5a97
TCG <ssibonokuhle@gmail.com>
2021-09-21 14:16:08 +0200

    Add API

--- backend/routes/api/user.js
+++ backend/routes/api/user.js
@@ -6,22 +6,26 @@ const auth = require("../auth");
 require("dotenv/config");
 
 const userRoutes = (app) => {
-  app.get("/user", async (req, res) => {
+  app.get("/api/user", auth, async (req, res) => {
     try {
-      const users = await User.find();
-      res.status(200).send(users);
+      const user = await User.findById({ _id: req.user._id }).select(
+        "-password"
+      );
+      res.status(200).send(user);
     } catch (error) {
       res.status(404).send(error);
     }
   });
-
   app.post("/register", async (req, res) => {
+    // valid before make a new user
     const { error } = registerValidation(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
+    //check if a user exists
     const user = await User.findOne({ email: req.body.email });
     if (user) return res.status(400).send(`Email already exists`);
 
+    //password hashing
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(req.body.password, salt);
 
@@ -29,26 +33,26 @@ const userRoutes = (app) => {
       const newUser = await User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: hashPassword,
       }).save();
-
       const token = jwt.sign({ id: newUser._id }, process.env.TOKEN_SECRET, {
         expiresIn: 3600,
       });
-
       res.status(201).json({ token, newUser });
     } catch (error) {
       res.status(400).send(error);
     }
   });
-
   app.post("/login", async (req, res) => {
+    //validating user
     const { error } = loginValidation(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
+    // check if user exists
     const user = await User.findOne({ email: req.body.email });
     if (!user) return res.status(400).send(`Email does not exist`);
 
+    // check if the password is correct
     const validPassword = await bcrypt.compare(
       req.body.password,
       user.password
@@ -56,9 +60,12 @@ const userRoutes = (app) => {
     if (!validPassword)
       return res.status(400).send("Email or password is wrong");
 
+    // create and assign a token
+
     const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET, {
       expiresIn: 3600,
     });
+    // res.header("auth-token", token).send(token);
     res.json({ token, user });
   });
 };
